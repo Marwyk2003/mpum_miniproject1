daec1aaba07770896eec33af1ccefa2285c4a64c
Marwyk2003 <marwyk2003@gmail.com>
2024-04-04 23:46:40 +0200

    Changes

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import numpy as np
-
-from regularizations import *
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class BaseModel:
@@ -15,12 +16,15 @@ class BaseModel:
         N, D = self.X.shape
         theta = np.zeros([D, 1])
         loss = 0
+        thetas = []
         for epoch in range(num_epochs):
-            ypred = self.pred(self.X, theta)
-            loss = self._mse(self.y, ypred, theta)
             gradient = self._grad(self.X, self.y, theta)
             theta = self._step(theta, gradient, step_size)
-        return theta, loss
+            ypred = self.pred(self.X, theta)
+            loss = self._mse(self.y, ypred, theta)
+            thetas += [theta]
+
+        return thetas, loss
 
     def _mse(self, y, ypred, theta):
         raise NotImplementedError
@@ -35,7 +39,7 @@ class BaseModel:
         raise NotImplementedError
 
 
-class LeastSquaresModel(BaseModel, BaseRegularization):
+class LeastSquaresModel(BaseModel):
     def _mse(self, y, ypred, _theta=None):
         return np.mean((y - ypred) ** 2)
 
@@ -51,55 +55,78 @@ class LeastSquaresModel(BaseModel, BaseRegularization):
         A = np.dot(A, self.X.T)
         A = np.dot(A, self.y)
         A = A.reshape([A.shape[0], 1])
-        return A, self._mse(self.y, self.pred(self.X, A), A)
+
+        ypred = self.pred(self.X, A)
+        loss = np.mean((self.y - ypred) ** 2)
+        return A, loss
 
 
-class RidgeLSModel(BaseModel, RidgeRegularization):
+class RidgeLSModel(BaseModel):
     def __init__(self, X, y, l):
-        super().__init__(X, y)
-        super(BaseModel, self).__init__(l)
-        self.reg = super(BaseModel, self)
+        self.X = X
+        self.y = y
+        self.l = l
 
     def _mse(self, y, ypred, theta):
-        return np.mean((y - ypred) ** 2) + self.cost(theta)
+        return np.sum((y - ypred) ** 2) / y.shape[0] + self.l * np.sum(theta[1:] ** 2)
 
     def _grad(self, X, y, theta):
-        return np.dot(X.T, self.pred(X, theta) - y) / y.shape[0] + self.grad(theta)
+        reg_grad = self.l * 2 * theta
+        return (np.dot(X.T, self.pred(X, theta) - y)) / y.shape[0] + reg_grad
 
     def _step(self, theta, grad, step_size):
         return theta - step_size * grad
 
     def analytical(self):
         A = np.dot(self.X.T, self.X)
-        A += self.l * np.identity(A.shape[0], np.int64)
+        A += np.identity(self.X.shape[1]) * self.l
         A = np.linalg.inv(A)
         A = np.dot(A, self.X.T)
         A = np.dot(A, self.y)
         A = A.reshape([A.shape[0], 1])
-        return A, self._mse(self.y, self.pred(self.X, A), A)
+
+        ypred = self.pred(self.X, A)
+        loss = np.mean((self.y - ypred) ** 2)
+        return A, loss
 
 
-class LassoLSModel(BaseModel, LassoRegularization):
+class LassoLSModel(BaseModel):
     def __init__(self, X, y, l):
-        super().__init__(X, y)
-        super(BaseModel, self).__init__(l)
-        self.reg = super(BaseModel, self)
+        self.X = X
+        self.y = y
+        self.l = l
 
     def _mse(self, y, ypred, theta):
-        return np.mean((y - ypred) ** 2) + self.reg.cost(theta)
+        return (np.sum((y - ypred) ** 2) + self.l * np.sum(np.abs(theta[1:])))/y.shape[0]
+
+    def _convert(self, ls_theta):
+        return np.sign(ls_theta) * np.maximum(np.abs(ls_theta) - self.l / 2, 0)
 
     def _grad(self, X, y, theta):
-        return np.dot(X.T, self.pred(X, theta)-y)/y.shape[0] + self.reg.grad(theta)
+        reg_grad = np.sign(theta)
+        reg_grad[0] = 0
+        return (np.dot(X.T, self.pred(X, theta) - y) + reg_grad) / y.shape[0]
 
     def _step(self, theta, grad, step_size):
         return theta - step_size * grad
 
+    # def train(self, num_epochs, step_size=0.1):
+    #     ls_model = LeastSquaresModel(self.X, self.y)
+    #     ls_thetas, _ = ls_model.train(num_epochs, step_size)
+    #     thetas = [self._convert(ls_theta) for ls_theta in ls_thetas]
+    #
+    #     theta = thetas[-1]
+    #     y_pred = self.pred(self.X, theta)
+    #     loss = self._mse(self.y, y_pred, theta)
+    #     return thetas, loss
+
     def analytical(self):
         ls_model = LeastSquaresModel(self.X, self.y)
         ls_theta, _ = ls_model.analytical()
-        theta =  np.sign(ls_theta)*np.maximum(np.abs(ls_theta)-self.l/2, 0)
-        y_pred = self.pred(self.X, theta)
-        cost = self._mse(self.y, y_pred, theta)
-        return theta, cost
 
-#%%
+        theta = self._convert(ls_theta)
+        y_pred = self.pred(self.X, theta)
+        loss = self._mse(self.y, y_pred, theta)
+        return theta, loss
+
+# %%
